e15bd09ab452416ec7ab3070ab03c5c591c45b45
Micheal Opeyemi <rgpolibliss@yahoo.com>
2023-04-03 19:26:01 -0700

    python_more_classes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,8 +9,8 @@ class Rectangle:
         """Initialize a new Rectangle.
 
         Args:
-        width (int): The width of the new rectangle.
-        height (int): The height of the new rectangle.
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
         """
         self.width = width
         self.height = height
